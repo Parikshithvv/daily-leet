3fcb876ab986d7a498ea7b708a4a1f37d9494d2a
Parikshith V <145186030+Parikshithvv@users.noreply.github.com>
2025-09-21 22:26:26 +0530

    Update 1408-find-the-smallest-divisor-given-a-threshold.java

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
@@ -47,3 +47,37 @@ class Solution {
         
     }
 }
+
+
+
+//example
+Input:
+
+nums = [1,2,5,9], threshold = 6
+
+
+getmax(nums) = 9
+
+Search space = [1..9]
+
+Binary Search Steps:
+
+mid = 5 → sum = ceil(1/5)+ceil(2/5)+ceil(5/5)+ceil(9/5) = 1+1+1+2 = 5 ≤ 6 ✅ (try smaller divisor)
+
+high = 4
+
+mid = 2 → sum = ceil(1/2)+ceil(2/2)+ceil(5/2)+ceil(9/2) = 1+1+3+5 = 10 > 6 ❌ (too small divisor)
+
+low = 3
+
+mid = 3 → sum = 1+1+2+3 = 7 > 6 ❌
+
+low = 4
+
+mid = 4 → sum = 1+1+2+3 = 7 > 6 ❌
+
+low = 5 → high = 4 → loop ends
+
+Answer = low = 5.
+
+✅ Smallest divisor = 5
