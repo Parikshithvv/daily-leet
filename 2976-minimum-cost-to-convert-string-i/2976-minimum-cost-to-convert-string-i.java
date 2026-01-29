304262d39659ea8c947dabd52c8db98255c8b110
Parikshith V <145186030+Parikshithvv@users.noreply.github.com>
2026-01-29 13:57:40 +0530

    Time: 23 ms (61.65%), Space: 47.9 MB (65.59%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
@@ -5,14 +5,13 @@ class Solution {
     void FW(long[][] arr, int[] cost, char[] original, char[] changed) {
         int n = original.length;
 
-        // Initialize direct transformations
+       
         for (int i = 0; i < n; i++) {
             int a = original[i] - 'a';
             int b = changed[i] - 'a';
             arr[a][b] = Math.min(arr[a][b], cost[i]);
         }
 
-        // Floyd–Warshall on 26 characters
         for (int k = 0; k < 26; k++) {
             for (int i = 0; i < 26; i++) {
                 for (int j = 0; j < 26; j++) {
@@ -34,7 +33,7 @@ class Solution {
 
         long[][] arr = new long[26][26];
 
-        // Initialize with large value (infinity)
+        
         for (int i = 0; i < 26; i++) {
             Arrays.fill(arr[i], 10000000);
         }
