d4c81be898513fabf1e075bb53123a5603a446d0
Parikshith V <145186030+Parikshithvv@users.noreply.github.com>
2026-04-10 18:30:48 +0530

    Time: 0 ms (100%), Space: 52.8 MB (81.91%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -3,28 +3,25 @@ class Solution {
 
         int n = nums.length;
 
-        if(n==1) return nums[0];
+        int low = 0;
+        int high = n - 1;
+    
 
-        if(nums[0] != nums[1]) return nums[0];
-        if(nums[n-1] != nums[n-2]) return nums[n-1];
-
-        int low=1 , high=n-2;
-
-        while(low <= high){
-            int mid = low+(high-low)/2;
-            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1]){
-                return nums[mid];
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (mid % 2 == 0 && nums[mid] == nums[mid + 1]){
+                low = mid + 2;
             }
-
-            if((mid % 2 == 1 && nums[mid] == nums[mid-1])
-            ||(mid % 2==0 && nums[mid] == nums[mid+1])) {
-
-                low = mid+1;            //to check left & right
+            else if (mid % 2 == 0 && nums[mid] != nums[mid + 1]){
+                high = mid;
+            }
+            else if(mid % 2 != 0 && nums[mid] == nums[mid-1]){
+                low = mid +1;
             }else{
-                high = mid-1;
+                high = mid;
             }
         }
-        return -1;
-        
+        return nums[low];
+
     }
 }
