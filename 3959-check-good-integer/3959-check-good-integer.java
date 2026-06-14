976730d5ff8a2ca3382851c47d3030b696bd4d8c
Parikshith V <145186030+Parikshithvv@users.noreply.github.com>
2026-06-14 11:39:34 +0530

    Time: 1 ms (95.3%), Space: 42.5 MB (72.52%) - LeetHub

---
+++ 3959-check-good-integer/3959-check-good-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkGoodInteger(int n) {
+        int sum1 = 0,sum2 = 0;
+        while(n>0){
+            int dig = n%10;
+            sum1 = sum1+dig;
+            sum2 = sum2 + (dig*dig);
+            n = n/10;
+
+        }
+        return sum2-sum1>=50;
+
+
+        
+    }
+}
